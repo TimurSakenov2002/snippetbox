9971fc3a9f926a12c013b4f9a1cc90c9c3378c37
timur <tsakienov@mail.ru>
2021-01-14 04:17:31 +0600

    handlers 2

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,26 +1,30 @@
 package main
 
 import (
+	"alexedwards.net/snippetbox/pkg/models"
+	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
+	"strconv"
 )
 
-func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
 		return
 	}
 
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := "7"
-
-	id, err := app.snippets.Insert(title, content, expires)
+	s, err := app.snippets.Get(id)
 	if err != nil {
-		app.serverError(w, err)
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	fmt.Fprintf(w, "%v", s)
 }
