723abf8839535f1778856b52ca7553583d208cc6
timur <tsakienov@mail.ru>
2021-01-14 04:18:47 +0600

    snippets2

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -3,31 +3,42 @@ package mysql
 import (
 	"alexedwards.net/snippetbox/pkg/models"
 	"database/sql"
-	"errors" // New import
+	"errors"
 )
 
 type SnippetModel struct {
 	DB *sql.DB
 }
 
-func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
+func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
 
 	stmt := `SELECT id, title, content, created, expires FROM snippets
-WHERE expires > UTC_TIMESTAMP() AND id = ?`
+WHERE expires > UTC_TIMESTAMP() ORDER BY created DESC LIMIT 10`
 
-	row := m.DB.QueryRow(stmt, id)
-
-	s := &models.Snippet{}
-
-	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	rows, err := m.DB.Query(stmt)
 	if err != nil {
-
-		if errors.Is(err, sql.ErrNoRows) {
-			return nil, models.ErrNoRecord
-		} else {
-			return nil, err
-		}
+		return nil, err
 	}
 
-	return s, nil
+	defer rows.Close()
+
+	snippets := []*models.Snippet{}
+
+	for rows.Next() {
+
+		s := &models.Snippet{}
+
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
