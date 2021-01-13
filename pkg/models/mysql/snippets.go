5dff1cda3653504a22f476c30de0a0033af2bb1e
timur <tsakienov@mail.ru>
2021-01-14 04:16:44 +0600

    snippets

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -1,25 +1,33 @@
 package mysql
 
-import "database/sql"
+import (
+	"alexedwards.net/snippetbox/pkg/models"
+	"database/sql"
+	"errors" // New import
+)
 
 type SnippetModel struct {
 	DB *sql.DB
 }
 
-func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
+func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
 
-	stmt := `INSERT INTO snippets (title, content, created, expires)
-VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+WHERE expires > UTC_TIMESTAMP() AND id = ?`
 
-	result, err := m.DB.Exec(stmt, title, content, expires)
+	row := m.DB.QueryRow(stmt, id)
+
+	s := &models.Snippet{}
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 	if err != nil {
-		return 0, err
+
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, models.ErrNoRecord
+		} else {
+			return nil, err
+		}
 	}
 
-	id, err := result.LastInsertId()
-	if err != nil {
-		return 0, err
-	}
-
-	return int(id), nil
+	return s, nil
 }
